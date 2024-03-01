71a59ede3bc305787a7ec1369c04fcb915fde569
caylof <caylof@qq.com>
2024-03-01 09:14:41 +0800

    modify: 对验证器添加了对空字符串的前置数据转换

--- app/validation/Form.php
+++ app/validation/Form.php
@@ -2,11 +2,6 @@
 
 namespace app\validation;
 
-use Illuminate\Filesystem\Filesystem;
-use Illuminate\Translation\FileLoader;
-use Illuminate\Translation\Translator;
-use Illuminate\Validation\Validator;
-
 abstract class Form
 {
     protected Validator $validator;
@@ -16,22 +11,23 @@ abstract class Form
 
     public function __construct()
     {
-        $translator = new Translator(
-            new FileLoader(new Filesystem(), base_path() . '/resource/translations'),
-            config('translation.locale', 'zh_CN')
-        );
         $this->validator = new Validator(
-            translator: $translator,
             data: [],
-            rules: $this->rules,
+            rules: [...$this->rules, ...$this->rules()],
             messages: $this->messages,
-            attributes: $this->attributes
+            customAttributes: $this->attributes
         );
     }
 
     public function validate(array $data): array
     {
+        $this->validator->covertEmptyStringToNull($data);
         $this->validator->setData($data);
         return $this->validator->validate();
+    }
+
+    protected function rules(): array
+    {
+        return [];
     }
 }
--- app/validation/Validator.php
+++ app/validation/Validator.php
@@ -9,12 +9,31 @@ use Illuminate\Translation\Translator;
 class Validator extends \Illuminate\Validation\Validator
 {
 
+    protected array $trimExceptFields = ['password'];
+
     public function __construct(array $data, array $rules, array $messages = [], array $customAttributes = [])
     {
         $translator = new Translator(
             new FileLoader(new Filesystem(), base_path() . '/resource/translations'),
             config('translation.locale', 'zh_CN')
         );
+        $this->covertEmptyStringToNull($data);
         parent::__construct($translator, $data, $rules, $messages, $customAttributes);
+    }
+
+    public function covertEmptyStringToNull(array &$arr)
+    {
+        foreach ($arr as $key => &$value) {
+            if (is_array($value)) {
+                $this->covertEmptyStringToNull($value);
+            } else {
+                if (is_string($value) && !in_array($key, $this->trimExceptFields)) {
+                    $value = preg_replace('~^[\s﻿]+|[\s﻿]+$~u', '', $value) ?? trim($value);
+                }
+                if (empty($value)) {
+                    $value = null;
+                }
+            }
+        }
     }
 }
